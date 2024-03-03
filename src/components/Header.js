b673dcc0b720bbf68b35528ecf8f683dd0c4c1cc
avainfo <avainfoconseil@gmail.com>
2024-03-03 08:07:57 +0100

    added <a> to header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,19 @@ import logo from "../assets/logo.png"
 import '../style/Header.scss'
 
 function Header() {
-    return (
-        <div className={"header"}>
-            <img src={logo} alt="logo"/>
-            <ul>
-                <li><u>Accueil</u></li>
-                <li>A Propos</li>
-            </ul>
-        </div>
-    );
+	return (
+		<div className={"header"}>
+			<img src={logo ?? ""} alt="logo"/>
+			<ul>
+				<li data-underline={true}>
+                    <a href={"/"}>Accueil</a>
+                </li>
+				<li data-underline={false}>
+                    <a href={"/"}>A Propos</a>
+                </li>
+			</ul>
+		</div>
+	);
 }
 
 export default Header;
