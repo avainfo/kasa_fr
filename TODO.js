1391346de1dc09f80dd280cb3f1bdc942163bbc2
avainfo <avainfoconseil@gmail.com>
2024-05-17 00:39:56 +0200

    Updated Todos

--- TODO.js
+++ TODO.js
@@ -2,4 +2,13 @@
  * TODO:
  *  - Remove logs
  *  - 404 Page
+ *  - go to last from first
+ *  - got to first from last
+ *  - check for solo pictures
+ *  - change arrows size
+ *  - replace section by semisection
+ *  - use real id of products
+ *  - relocate to 404 if id != max id of doc
+ *  - change css des link homes page
+ *  - useParams
  */
