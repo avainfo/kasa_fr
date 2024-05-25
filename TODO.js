0ed995935380eb62e5fb4e9de7e648bb93ac2c22
BuildTools <unconfigured@null.spigotmc.org>
2024-05-26 01:59:05 +0200

    Updated TODOS : replaced Section by Semi-section

--- TODO.js
+++ TODO.js
@@ -5,7 +5,6 @@
  *  - got to first from last
  *  - check for solo pictures
  *  - change arrows size
- *  - replace section by semisection
  *  - use real id of products
  *  - relocate to 404 if id != max id of doc
  *  - useParams
